89553f562cd19f0253ba23bb6fd29370763b39a9
GO.DRE <go.dre@godres-macbook-pro.local>
2018-12-05 15:21:01 +0300

    Initial Commit

---
+++ json_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  json_app
+//
+//  Created by GO.DRE on 5.12.2018.
+//  Copyright © 2018 com.ceren.ozgur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
